590ac9605ba099581c9609a928fe7a8192b01246
luansramos <luanramosgarciadesouza014@gmail.com>
2024-09-18 03:20:03 -0300

    fazendo pagina do ao vivo

--- formula-e/src/pages/Aovivo/Aovivo.jsx
+++ formula-e/src/pages/Aovivo/Aovivo.jsx
@@ -1,7 +1,83 @@
+import "./Aovivo.css"
 
-export default function Live(){
+function Live(){
     return(
         
-        <h1> Live Page</h1>
-    )
-}
+    <main className="rolagem">
+      <div className="container">
+        <h1>Transmissão Ao Vivo</h1>
+        <div id="live-stream" className="live-stream">
+          <iframe 
+            id="live-video" 
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/live_stream?channel=YOUR_CHANNEL_ID" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullScreen
+          ></iframe>
+        </div>
+        <h2>Outras Corridas/Outros Vídeos</h2>
+        <div id="videos" className="video-list">
+          <div className="video">
+            <iframe 
+              width="560" 
+              height="315" 
+              src="https://www.youtube.com/embed/ZUTiqPcJMj8?si=qNQLbqUPE-mLqFvC&amp;start=1" 
+              title="YouTube video player" 
+              frameBorder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div className="video">
+            <iframe 
+              width="560" 
+              height="315" 
+              src="https://www.youtube.com/embed/wPtoSGoDGG0?si=GOep-wf2_Sbxvm6r" 
+              title="YouTube video player" 
+              frameBorder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div className="video">
+            <iframe 
+              width="560" 
+              height="315" 
+              src="https://www.youtube.com/embed/-LSXZ0xLgRM?si=vxksuyonk_xO-VoK" 
+              title="YouTube video player" 
+              frameBorder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div className="video">
+            <iframe 
+              width="560" 
+              height="315" 
+              src="https://www.youtube.com/embed/i0gxQ-4pKAs?si=PwntgnAxMQBvbMsS&amp;start=3" 
+              title="YouTube video player" 
+              frameBorder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div className="video">
+            <iframe 
+              width="560" 
+              height="315" 
+              src="https://www.youtube.com/embed/FLMLjZyTeZ8?si=IGhpJF5WK9oE9gFA" 
+              title="YouTube video player" 
+              frameBorder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              allowFullScreen
+            ></iframe>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Live;
--- formula-e/src/pages/Noticias/Noticias.jsx
+++ formula-e/src/pages/Noticias/Noticias.jsx
@@ -1,51 +1,65 @@
-function Noticias(){
-    return(
-        <>
-        {/* TROCAAAAAR O LINK O A HREF PELO LINK */}
-        <section className="noticia-1 rolagem">
-        <div className="center">
-            <div className="noticias-wraper-1">
-                <div className="noticia-destaque">
-                    <a href="#">
-                        <div style="background-image: url('../img/Not.perfume.webp');" className="noticia-destaque-single">
-                            <h3>PERFUME COM CHEIRO DE PNEU?</h3>
+import "./noticias.css"
+
+
+function Noticias() {
+
+
+    return (
+
+        <div className="noticia-1 rolagem">
+            <div className="center">
+                <div className="noticias-wraper-1">
+                    <div className="noticia-destaque">
+                        <a href="#!">
+                            <div
+                                className="noticia-destaque-single"
+                                style={{ backgroundImage: 'url()' }} // Adicione a URL correta da imagem aqui.
+                            >
+                                <h3>PERFUME COM CHEIRO DE PNEU?</h3>
+                            </div>
+                        </a>
+                    </div>
+
+                    <div className="outras-noticias-1">
+                        <div
+                            className="outras-noticias-1-single"
+                            style={{ backgroundImage: 'url()' }} // Adicione a URL correta da imagem aqui.
+                        >
+                            <a href="#!">
+                                <h3>Criação de Circuito</h3>
+                            </a>
                         </div>
-                    </a>
-                </div>
 
-                <div className="outras-noticias-1">
-                    <div style="background-image: url('../img/Not.Cirquito.webp');" className="outras-noticias-1-single">
-                        <a href="#">
-                            <h3>Criação de Circuito</h3>
-                        </a>
-                    </div>
+                        <div
+                            className="outras-noticias-1-single"
+                            style={{ backgroundImage: 'url()' }} // Adicione a URL correta da imagem aqui.
+                        >
+                            <a href="#!">
+                                <h3>Herança Brasileira no Automobilismo</h3>
+                            </a>
+                        </div>
 
-                    <div style="background-image: url('../img/Not.herança-Br.webp');" className="outras-noticias-1-single">
-                        <a href="#">
-                            <h3>Herança Brasileira no Automobilismo</h3>
-                        </a>
-                    </div>
+                        <div
+                            className="outras-noticias-1-single"
+                            style={{ backgroundImage: 'url()' }} // Adicione a URL correta da imagem aqui.
+                        >
+                            <a href="#!">
+                                <h3>Da Costa Defende Título</h3>
+                            </a>
+                        </div>
 
-                    <div style="background-image: url('../img/Not.dacostadefende.jpg');" className="outras-noticias-1-single">
-                        <a href="#">
-                            <h3>Da Costa Defende Título</h3>
-                        </a>
-                    </div>
-
-                    <div style="background-image: url('../img/Not.lucas-volta.jpeg');" className="outras-noticias-1-single">
-                        <a href="#">
-                            <h3>Lucas di Grassi Volta à China</h3>
-                        </a>
+                        <div
+                            className="outras-noticias-1-single"
+                            style={{ backgroundImage: 'url()' }} // Adicione a URL correta da imagem aqui.
+                        >
+                            <a href="#!">
+                                <h3>Lucas di Grassi Volta à China</h3>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </section>
-
-
-
-
-        </>
     );
 };
 
