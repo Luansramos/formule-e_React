3a5b5fe81ecffeabb6f127d6ca3494408447f1c6
Luansramos <luanramosgarciadesouza014@gmail.com>
2024-09-20 21:45:02 -0300

    apagando pastas inuteis

--- formula-e/src/main.jsx
+++ formula-e/src/main.jsx
@@ -1,37 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import Times from './pages/Times/Times.jsx'
-import Home from './pages/Home/Home.jsx'
-import Corridas from './pages/Corridas/Corridas.jsx'
-import Noticias from './pages/Noticias/Noticias.jsx'
-import Jogo from './pages/Jogo/Jogo.jsx'
-import Loja from './pages/Loja/Loja.jsx'
 import Live from './pages/Aovivo/Aovivo.jsx'
 import Contato from './pages/Contato/Contato.jsx'
-import Login from './pages/Login/Login.jsx'
+import Corridas from './pages/Corridas/Corridas.jsx'
+import Home from './pages/Home/Home.jsx'
 import SaibaMais from './pages/Home/SaibaMais.jsx'
+import Jogo from './pages/Jogo/Jogo.jsx'
+import Login from './pages/Login/Login.jsx'
+import Loja from './pages/Loja/Loja.jsx'
+import Noticias from './pages/Noticias/Noticias.jsx'
+import Times from './pages/Times/Times.jsx'
 
 
 
 const router = createBrowserRouter([
-  
-  { 
-    path:'/',
-    element: <App/>,
+
+  {
+    path: '/',
+    element: <App />,
     children: [
-      {index:true,element:<Home/>},
-      {path:'times',element:<Times/>},
-      {path:'Corridas',element:<Corridas/>},
-      {path:'Noticias',element:<Noticias/>},
-      {path:'Jogo',element:<Jogo/>},
-      {path:'Loja',element:<Loja/>},
-      {path:'Live',element:<Live/>},
-      {path:'Contato',element:<Contato/>},
-      {path:'Login',element:<Login/>},
-      {path:'SaibaMais', element:<SaibaMais/>}
+      { index: true, element: <Home /> },
+      { path: 'times', element: <Times /> },
+      { path: 'Corridas', element: <Corridas /> },
+      { path: 'Noticias', element: <Noticias /> },
+      { path: 'Jogo', element: <Jogo /> },
+      { path: 'Loja', element: <Loja /> },
+      { path: 'Live', element: <Live /> },
+      { path: 'Contato', element: <Contato /> },
+      { path: 'Login', element: <Login /> },
+      { path: 'SaibaMais', element: <SaibaMais /> }
     ]
   }
 
@@ -41,7 +41,7 @@ createRoot(document.getElementById('root')).render(
 
 
   <StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
 
